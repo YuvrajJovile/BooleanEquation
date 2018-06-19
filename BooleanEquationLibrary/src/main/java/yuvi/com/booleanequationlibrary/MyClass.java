92ec84c4007758bd6b90eeda46aa042817e4bda6
Yuvaraj <yuvaraj.n@askpundit.com>
2018-06-19 23:05:01 +0530

    Initial commit

---
+++ BooleanEquationLibrary/src/main/java/yuvi/com/booleanequationlibrary/MyClass.java
@@ -0,0 +1,14 @@
+package yuvi.com.booleanequationlibrary;
+
+public class MyClass {
+
+    public static void main(String[] args){
+        printMessage("Hello World");
+    }
+
+
+    private static void printMessage(String pMessage){
+        System.out.println(pMessage);
+    }
+
+}
